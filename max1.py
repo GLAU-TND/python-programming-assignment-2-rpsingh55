f34a32591e2da37f2c4a745c647bbb12af7bb634
rpsingh55 <60340745+rpsingh55@users.noreply.github.com>
2020-02-20 20:56:25 +0530

    Rpsingh

---
+++ max1.py
@@ -0,0 +1,18 @@
+n=int(input())
+p=bin(n)
+s,c=p[2:],[0]
+for i in range(len(s)):
+	d=0
+	if s[i] =="1":
+		for j in range(i,len(s)):
+			if s[j] =="1":
+				d+=1
+				if j==len(s)-1:
+					c.append(d)
+					break
+			
+			else:
+				c.append(d)
+				break
+print( max(c))
+
